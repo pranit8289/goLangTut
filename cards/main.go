465321668f573a282f1627ec42570148e079e83f
Pranit Kokate <pranit.py9@gmail.com>
2023-05-20 22:59:55 +0530

    IT-2 cards - usage of function

--- cards/main.go
+++ cards/main.go
@@ -6,8 +6,14 @@ func main() {
 	fmt.Println("Welcome to Cards...!")
 	// var card string = "Ace of Spade" first method to declare variable
 	// var card = "Ace of Spade" second method to declare variable
-	card := "Ace of Spade" // third method to declare variable
+	// card := "Ace of Spade" // third method to declare variable
 	// use this at very first initialization of variable
-	var card float = 10.0
+	// var card float = 10.0
+	card := newCard()
+
 	fmt.Println("card :", card)
+}
+
+func newCard() string {
+	return "Five of Dimonds"
 }
