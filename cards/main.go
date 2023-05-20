c92339f0a81064f011dacff5c52617a64a0df44a
Pranit Kokate <pranit.py9@gmail.com>
2023-05-20 22:45:37 +0530

    IT-2 - cards - variable declaration methods

---
+++ cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Cards...!")
+	// var card string = "Ace of Spade" first method to declare variable
+	// var card = "Ace of Spade" second method to declare variable
+	card := "Ace of Spade" // third method to declare variable
+	// use this at very first initialization of variable
+	var card float = 10.0
+	fmt.Println("card :", card)
+}
